488a4b34b5c78cbb9b35d42409fbe71945d5cd0b
Danteps-4 <danteaugsburger4@gmail.com>
2026-01-01 11:08:36 -0300

    select

--- app/services/tiendanube.py
+++ app/services/tiendanube.py
@@ -12,6 +12,7 @@ load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
+from sqlmodel import select
 from app.database import engine, get_session
 from app.models import TiendaNubeToken, Store, User
 from app.security import encrypt_token, decrypt_token
